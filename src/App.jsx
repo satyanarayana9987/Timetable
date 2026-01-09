403b230bfaef7917733d7909553c0ee2323ac756
M.satyanarayana <2500030164@klunivertsity.in>
2026-01-09 10:46:33 +0530

    Initial commit - Timetable project

---
+++ src/App.jsx
@@ -0,0 +1,97 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h2>Class Time Table</h2>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Day</th>
+            <th>1</th>
+            <th>2</th>
+            <th>3</th>
+            <th>4</th>
+            <th>5</th>
+            <th>6</th>
+            <th>7</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Mon</td>
+            <td>25EC2101E-L<br />RoomNo-L303</td>
+            <td>25EC2101E-L<br />RoomNo-L303</td>
+            <td>25SC1204E-S<br />RoomNo-L303</td>
+            <td>25SC1204E-S<br />RoomNo-L303</td>
+            <td>25CS1201E-S<br />RoomNo-L303</td>
+            <td>25CS1201E-S<br />RoomNo-L303</td>
+            <td>-</td>
+          </tr>
+
+          <tr>
+            <td>Tue</td>
+            <td>25MT1205E-L<br />RoomNo-L303</td>
+            <td>25MT1205E-L<br />RoomNo-L303</td>
+            <td>25SC1204E-S<br />RoomNo-L303</td>
+            <td>25SC1204E-S<br />RoomNo-L303</td>
+            <td>25CS1201E-S<br />RoomNo-L303</td>
+            <td>25CS1201E-S<br />RoomNo-L303</td>
+            <td>-</td>
+          </tr>
+
+          <tr>
+            <td>Wed</td>
+            <td>25SC1204E-L<br />RoomNo-L303</td>
+            <td>25SC1204E-L<br />RoomNo-L303</td>
+            <td>25EC2101E-S<br />RoomNo-L303</td>
+            <td>25EC2101E-S<br />RoomNo-L303</td>
+            <td>25UC1204E-P<br />RoomNo-L303</td>
+            <td>25UC1204E-P<br />RoomNo-L303</td>
+            <td>-</td>
+          </tr>
+
+          <tr>
+            <td>Thu</td>
+            <td>25SC1204E-P<br />RoomNo-L303</td>
+            <td>25SC1204E-P<br />RoomNo-L303</td>
+            <td>25EC2101E-S<br />RoomNo-L303</td>
+            <td>25EC2101E-S<br />RoomNo-L303</td>
+            <td>25UC1204E-P<br />RoomNo-L303</td>
+            <td>25UC1204E-P<br />RoomNo-L303</td>
+            <td>-</td>
+          </tr>
+
+          <tr>
+            <td>Fri</td>
+            <td>25MT1205E-P<br />RoomNo-L303</td>
+            <td>25MT1205E-P<br />RoomNo-L303</td>
+            <td>25CS1201E-P<br />RoomNo-L303</td>
+            <td>25CS1201E-P<br />RoomNo-L303</td>
+            <td>25SC1204E-S<br />RoomNo-L303</td>
+            <td>25SC1204E-S<br />RoomNo-L303</td>
+            <td>-</td>
+          </tr>
+
+          <tr>
+            <td>Sat</td>
+            <td>25MT1205E-P<br />RoomNo-L303</td>
+            <td>25MT1205E-P<br />RoomNo-L303</td>
+            <td>25CS1201E-P<br />RoomNo-L303</td>
+            <td>25CS1201E-P<br />RoomNo-L303</td>
+            <td>25SC1204E-S<br />RoomNo-L303</td>
+            <td>25SC1204E-S<br />RoomNo-L303</td>
+            <td>-</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
